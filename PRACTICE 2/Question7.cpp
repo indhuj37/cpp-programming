10ca83efe711ada2487c28e18c0939a14536c342
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-08-11 21:20:32 +0530

    Add files via upload

---
+++ PRACTICE 2/Question7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a;
+    int b;
+    int c;
+    cout<<"Enter the value for a"<<endl;
+    cin>>a;
+    cout<<"Enter the value for b"<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"The addition of a and b is c "<<c<<endl;
+}
