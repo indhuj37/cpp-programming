8aae9668723137f06fdf3ab56eabbc6c7c2384c3
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-08-12 21:39:02 +0530

    Add files via upload

---
+++ PRACTICE 2/Question9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float a;
+float b;
+float c;
+float d;
+{
+cout<<"Enter the value for a"<<endl;
+cin>>a;
+cout<<"Enter the value for b"<<endl;
+cin>>b;
+cout<<"Enter the value for c"<<endl;
+cin>>c;
+d=(a+b+c)/3;
+cout<<"The average of a,b and c is d"<<d<<endl;
+}
+}
